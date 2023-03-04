fc426a5f812c733dacda8d23d24002982394552f
Benderulas <krytoikazak@mail.ru>
2023-03-04 18:19:31 +0200

    [C#] add ChatGPTController

---
+++ BookWriter/src/ChatGPTController.cs
@@ -0,0 +1,41 @@
+﻿using OpenAI.GPT3;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+
+namespace BookWriter.src
+{
+    class ChatGPTController : IChatGPTController
+    {
+        private OpenAIService _sOpenAiService;
+
+        public ChatGPTController(string userSecretKey)
+        {
+            var openAiOptions = new OpenAiOptions();
+            openAiOptions.ApiKey = userSecretKey;
+
+            _sOpenAiService = new OpenAIService(openAiOptions);
+            _sOpenAiService.SetDefaultModelId(Models.ChatGpt3_5Turbo0301);
+        }
+
+        public async Task<string> SendMessage(string message)
+        {
+            var completionResult = await _sOpenAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+            {
+                Messages = new List<ChatMessage>
+                {
+                    ChatMessage.FromUser(message),
+                },
+                Model = Models.ChatGpt3_5Turbo
+            });
+
+            if (completionResult.Successful)
+            {
+                return completionResult.Choices.First().Message.Content;
+            } else
+            {
+                throw new Exception("Some error Occured");
+            }
+        }
+    }
+}
---
+++ BookWriter/src/IChatGPTController.cs
@@ -0,0 +1,7 @@
+﻿namespace BookWriter.src
+{
+    interface IChatGPTController
+    {
+        public Task<string> SendMessage(string message);
+    }
+}
